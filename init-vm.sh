c5d41f9beb732c321810adccca9ab0326b370b1f
Laszlo Vadasz <laszlo.vadasz@adgistics.com>
2016-01-08 15:37:11 +0000

    fix and add extra check to the init-vm script

--- init-vm.sh
+++ init-vm.sh
@@ -3,22 +3,26 @@
 WEB_DIR="web"
 PROJECT_NAME=${PWD##*/}
 
-# create new VM
-docker-machine create -d virtualbox $(PROJECT_NAME)
+if ! docker-machine ls | grep -q "$PROJECT_NAME"; then
+    echo -e "- create new VM - '$PROJECT_NAME'\n"
+    docker-machine create -d virtualbox "$PROJECT_NAME"
 
-docker-machine stop $(PROJECT_NAME)
+    if ! [[ $PWD == "/c/Users/"* ]]; then     # docker add /c/Users by default
+        docker-machine stop "$PROJECT_NAME"
 
-# add share folder
-#/c/Program\ Files/Oracle/VirtualBox/
-VBoxManage sharedfolder add dev --hostpath "$(pwd)/$(WEB_DIR)" --name $(WEB_DIR) --automount
+        echo -e "\n- Add share folder to the VM\n"
 
-# mount share folder
-docker-machine.exe ssh dev "sudo mkdir -p /$(WEB_DIR) && sudo mount -t vboxsf -o uid=1000,gid=1000 $(WEB_DIR) /$(WEB_DIR)"
+        # Add VirtualBox default location to the path
+        export PATH="/c/Program Files/Oracle/VirtualBox:/Applications/VirtualBox.app/Contents/MacOS:$PATH"
+        VBoxManage sharedfolder add "$PROJECT_NAME" --hostpath "$PWD/$WEB_DIR" --name "$WEB_DIR" --automount
 
-docker-machine start $(PROJECT_NAME)
+        docker-machine start "$PROJECT_NAME"
 
-# activate VM
-eval "$(docker-machine env )$($PROJECT_NAME)"
+        echo -e "\n- Mount share folder\n"
+        docker-machine ssh "$PROJECT_NAME" "sudo mkdir -p $PWD/$WEB_DIR && sudo mount -t vboxsf -o uid=1000,gid=1000 $WEB_DIR $PWD/$WEB_DIR"
+    fi
+fi
 
 
-docker-machine.exe ssh dev "sudo mkdir -p /web && sudo mount -t vboxsf -o uid=1000,gid=1000 web $(pwd)/web"
+echo -e "\n\nYou can setup the terminal environment with this command:"
+echo -e "eval \"\$(docker-machine env --shell bash $PROJECT_NAME)\""
